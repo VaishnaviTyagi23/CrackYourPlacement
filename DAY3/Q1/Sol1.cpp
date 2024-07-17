1137d132c96368ac9acf2cca0ecfe3eb402787e5
VT <157911088+VaishnaviTyagi23@users.noreply.github.com>
2024-07-17 20:56:04 +0530

    Find-the-index-of-the-first-occurrence-in-a-string

---
+++ DAY3/Q1/Sol1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+      int i,count;
+        for(i=0;i<haystack.length();i++){
+           count=0;
+           if(haystack[i]==needle[0]){
+            for(int j=0;j<needle.length();j++){
+              if(haystack[i+j]==needle[j]){
+                count++;
+              }
+              else break ;
+          }
+          if(count==needle.length()){
+          return i;
+        }
+        }
+        }  
+        return -1;
+    }
+};
